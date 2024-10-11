6558e1857d4683c16f4a8b4897afdb24ccc0b31c
Hanss53 <140678383+Hanss53@users.noreply.github.com>
2024-10-11 10:53:04 +0700

    initial project

---
+++ Assets/Asset Resources/Script/Scene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
